c69b719310aa2764fe356a732a4ce8bc8a937009
MedMaychin <kimo.darkness@gmail.com>
2022-08-15 15:38:12 +0100

    rect

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Create from './components/create';
+
+function App() {
+  return (
+    <div className="container">
+      <h2 className='d-flex p-2 justify-content-center'>Learn CRUD using React JS</h2>
+      <Create />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/create.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const Create = () => {
+  return (
+    <>
+
+      <form>
+        <div className="mb-3">
+          <label className="form-label">Name</label>
+          <input
+            type="text" className="form-control"
+          />
+        </div>
+        <div className="mb-3">
+          <label className="form-label">Email address</label>
+          <input
+            type="email" className="form-control"
+            aria-describedby="emailHelp"
+          />
+        </div>
+ 
+
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
+
+
+
+    </>
+  )
+}
+
+export default Create
